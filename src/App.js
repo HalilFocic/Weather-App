f92dfe3610521811f0945bec18577a2d90ac6298
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2020-12-31 22:41:02 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+const key="9089186cf58c3f6edb2715df96895f63";
+const link='https://www.superheroapi.com/api.php/3509433779090013/search/Aquaman';
+class App extends Component{
+  render(){
+    fetch(link, {
+      crossDomain:true,
+      method: 'GET',
+      headers: {'Content-Type':'application/json'},
+      
+    })
+    .then(response=>response.json())
+    .then(responseJson =>{console.log(responseJson)})
+    return(
+    <div className="App">Hello</div>
+      );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
 }
 
 export default App;
