b340454e50cdee9f4b4ec504d31b0100f5acfd37
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2020-12-31 23:54:22 +0100

    -all icons added

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { TiWeatherCloudy } from "react-icons/ti";
 import { IoCloudSharp } from "react-icons/io5";
+import { FiWind } from "react-icons/fi";
+import { FaTemperatureHigh } from "react-icons/fa";
+import { HiLocationMarker } from "react-icons/hi";
 const WeatherApp = (result) => {
   const forecast = result.weather[0].main;
   const temparature = result.main.temp;
@@ -9,9 +12,18 @@ const WeatherApp = (result) => {
   return (
     <>
       <h4>{forecast === "Clouds" ? <IoCloudSharp /> : "Nisu oblaci"}</h4>
-      <h4>{temparature}</h4>
-      <h4>{wind}</h4>
-      <h4>{city}</h4>
+      <h4>
+        <FaTemperatureHigh />
+        {temparature}
+      </h4>
+      <h4>
+        <FiWind />
+        {wind}
+      </h4>
+      <h4>
+        <HiLocationMarker />
+        {city}
+      </h4>
     </>
   );
 };
